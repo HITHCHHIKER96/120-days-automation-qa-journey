9b527f0e16286019175541297caa4391df789419
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-11 21:07:40 +0530

    Update and rename 22.random to 22.random.py

---
+++ 📂 python-basics/22.random.py
@@ -0,0 +1,7 @@
+import random
+
+# print(help(random))
+num1 = 1
+num2 = 1000
+print(random.randint(num1, num2)) # to print random integer
+print(random.random()) # to print random floating point number
