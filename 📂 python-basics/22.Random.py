98e530654f8a01e257fdce41c2c2003dae1ef017
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-11 21:56:16 +0530

    Update 22.Random.py

--- 📂 python-basics/22.Random.py
+++ 📂 python-basics/22.Random.py
@@ -4,7 +4,9 @@ import random
 num1 = 1
 num2 = 1000
 options = ("rock", "paper", "scissors")
-print(random.randint(num1, num2)) # to print random integer
-print(random.random()) # to print random floating point number
+cards = ["2","3","4","5","6","7","8","9","10","K","Q","J","A"]
+# print(random.randint(num1, num2)) # to print random integer
+# print(random.random()) # to print random floating point number
+print(random.shuffle(cards)) # it will shuffle cards
 
 print(random.choice(options)) # this choice() method is best for games
