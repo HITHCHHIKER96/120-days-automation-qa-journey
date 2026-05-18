f6e37bb5ad82f3e2abadcd4ea37f012d5a5902d6
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-18 19:34:24 +0530

    Update 22.Random.py

--- 📂 python-basics/22.Random.py
+++ 📂 python-basics/22.Random.py
@@ -8,5 +8,5 @@ cards = ["2","3","4","5","6","7","8","9","10","K","Q","J","A"]
 # print(random.randint(num1, num2)) # to print random integer
 # print(random.random()) # to print random floating point number
 print(random.shuffle(cards)) # it will shuffle cards
-
+print(help(random))
 print(random.choice(options)) # this choice() method is best for games
