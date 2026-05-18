637f6314933498c0f65c5cdc8d8473d69bb6d6ca
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-18 20:45:29 +0530

    Update 25.function.py

--- 📂 python-basics/25.function.py
+++ 📂 python-basics/25.function.py
@@ -1,11 +1,20 @@
 # function = A block of reusable code
              # place () after the function name invoke it
+             # So when you invoke parameter inside the function you have to invoke matching number of parameters inside the function when calling
 
-def korean_beauty():
-    print("They are Fucking beautiful!")
-    print("They are Cute!")
+
+def korean_beauty(name, Beauty):
+    print(f"{name} are Fucking beautiful!")
+    print(f"Her {Beauty} is damn!")
     print("Their language is kind of good")
 
-korean_beauty()
-korean_beauty()
-korean_beauty()
+korean_beauty("Eblin", "Beauty")
+
+# return = statement used to end a function
+           # and send a result back to the caller
+
+def multiply(a, b):
+    answer = a * b
+    print(answer)
+
+multiply(56,56)
