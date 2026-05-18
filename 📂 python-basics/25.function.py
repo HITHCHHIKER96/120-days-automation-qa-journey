a5eebc332bd50b97e8b911bab739561eca739dc1
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-18 20:29:38 +0530

    Update 25.function.py

--- 📂 python-basics/25.function.py
+++ 📂 python-basics/25.function.py
@@ -1,1 +1,11 @@
+# function = A block of reusable code
+             # place () after the function name invoke it
 
+def korean_beauty():
+    print("They are Fucking beautiful!")
+    print("They are Cute!")
+    print("Their language is kind of good")
+
+korean_beauty()
+korean_beauty()
+korean_beauty()
