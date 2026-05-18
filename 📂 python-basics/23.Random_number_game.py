e68240cd431b2d32d5c42ec0ad235937397fff54
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-18 19:08:51 +0530

    Update 23.Random_number_game.py

--- 📂 python-basics/23.Random_number_game.py
+++ 📂 python-basics/23.Random_number_game.py
@@ -1,1 +1,8 @@
+import random
 
+lowest_number = 1
+highest_number = 100
+
+user_num = int(input("Enter the number you want: "))
+if user_num>1 and user_num<100:
+    print(random.shuffle(user_num))
