77cf4b0bedf334c371a064f366eddbe0d0746b95
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-20 22:17:45 +0530

    Update Palindrome.py

--- 📂 python-basics/Python_Practice/Palindrome.py
+++ 📂 python-basics/Python_Practice/Palindrome.py
@@ -1,1 +1,11 @@
 Check if a string is a palindrome
+
+def palindrome():
+    num = int(input("Enter the number you want: "))
+    reverse = 0
+    original = num
+    while num > 0:
+        reverse = reverse * 10 + (num % 10)
+        num //= 10
+    return original == reverse
+print(palindrome())
