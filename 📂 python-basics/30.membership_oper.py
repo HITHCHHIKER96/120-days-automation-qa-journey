9f3a5f91f8c5e930a9310580e46d263d12473a33
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-30 19:24:38 +0530

    Update 30.membership_oper.py

--- 📂 python-basics/30.membership_oper.py
+++ 📂 python-basics/30.membership_oper.py
@@ -1,1 +1,27 @@
+# Membership Operator = used to test whether a value or variable is found in a sequence
+                    #   (string, list, tuple, set or dictionary)
+                    #   1.in
+                    #   2.not in
+# word = "KOREA"
+#
+# letter = input("Enter the secret word: ")
+#
+# if letter in word:
+#     print(f"There is a {letter}")
+# else:
+#     print(f"The {letter} was not found")
 
+# Another example:
+another_dict ={
+    "Pritam": 34,
+    "Rajdeep": 56,
+    "Goutam": 29,
+    "Raju": 75
+}
+
+word = input("Enter the name for that dictionary: ")
+
+if word in another_dict.items():
+    print(f"The {word} is not in {another_dict[word]}")
+else:
+    print(f"The {word} is inside {another_dict[word]}")
