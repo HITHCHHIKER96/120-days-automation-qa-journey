16f499937762b53ff3fbf39b5a5071debb30f184
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-05-30 11:57:14 +0530

    Update 29.Iterables.py

--- 📂 python-basics/29.Iterables.py
+++ 📂 python-basics/29.Iterables.py
@@ -1,1 +1,39 @@
+#Iterables = An object/collection that can return its element one at a time,
+            # allowing it to be iterated over in a loop.
+
+#1. Standard Sequence Iteration (Lists, Tuples, Strings)
+# Iterating over a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# Iterating over a string
+word = "Python"
+for letter in word:
+    print(letter)
+
+#Dictionary Iteration
+user = {"name": "Alice", "role": "Admin", "id": 404}
+
+# Iterate over keys (default)
+for key in user:
+    print(key)
+
+# Iterate over values
+for value in user.values():
+    print(value)
+
+# Iterate over key-value pairs (items)
+for key, value in user.items():
+    print(f"{key}: {value}")
+
+#Behind the Scenes: How Iterables Work
+numbers = [10, 20]
+
+# Manually mimicking a for loop
+my_iterator = iter(numbers)
+
+print(next(my_iterator))  # Output: 10
+print(next(my_iterator))  # Output: 20
+# print(next(my_iterator)) # This would raise StopIteration exception
 
