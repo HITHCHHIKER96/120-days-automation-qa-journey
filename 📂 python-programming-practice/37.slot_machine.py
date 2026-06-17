d4bce9e9b7c0c1ae7426777edc861630db776304
Pritam Ghosh <108845696+HITHCHHIKER96@users.noreply.github.com>
2026-06-17 13:26:39 +0530

    Update 37.slot_machine.py

--- 📂 python-programming-practice/37.slot_machine.py
+++ 📂 python-programming-practice/37.slot_machine.py
@@ -1,1 +1,29 @@
+# Python Slot Machine
 
+def spin_row():
+    pass
+
+def print_row():
+    pass
+
+def get_payout():
+    pass
+
+def main():
+    balance = 100
+
+    print("********************")
+    print(" Welcome to Slot machine game ")
+    print("Symbols: 🍒 🍉 🥭 🔔 ")
+    print("******************")
+
+    while balance > 0:
+        print(f"Current Balance:{balance}")
+        bet = input("Place your bet amount: ")
+
+        if  not bet.isdigit():
+            print("Please enter a valid number.")
+            continue
+
+if __name__ == '__main__':
+    main()
